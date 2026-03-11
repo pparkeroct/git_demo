a35587330f0cc210acd10f5a61eb53ec5fe9c54b
Patrick Parker <pparker@octaviansolutions.com>
2026-03-11 12:34:30 -0600

    Add christmas tree

--- HelloWorld.py
+++ HelloWorld.py
@@ -3,12 +3,26 @@ import sys
 def easter_egg():
     print("You found the Easter Egg! 🐰")
 
+def christmas_tree():
+    tree = [
+        "   *   ",
+        "  ***  ",
+        " ***** ",
+        "*******",
+        "  ***  ",
+        "  ***  "
+    ]
+    for line in tree:
+        print(line)
+
 def main():
     message = "Hello, World!"
     if len(sys.argv) > 1:
         message = sys.argv[1]
     if message.lower() == "easter_egg":
         easter_egg()
+    elif message.lower() == "christmas_tree":
+        christmas_tree()
     else:
         print(message)
 
