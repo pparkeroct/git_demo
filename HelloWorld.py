ecc8df907952bbfedd8273b1ac403e3f5a873595
Patrick Parker <pparker@octaviansolutions.com>
2026-03-11 12:30:35 -0600

    Add easter egg

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,10 +1,16 @@
 import sys
 
+def easter_egg():
+    print("You found the Easter Egg! 🐰")
+
 def main():
     message = "Hello, World!"
     if len(sys.argv) > 1:
         message = sys.argv[1]
-    print(message)
+    if message.lower() == "easter egg":
+        easter_egg()
+    else:
+        print(message)
 
 if __name__ == "__main__":
     main()
